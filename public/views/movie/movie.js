8ececad248095df4d96318a0c4d720a53d175dee
Jerry Pistorius <jpistorius1@gmail.com>
2015-06-20 16:59:45 -0600

    Change routes

--- public/views/movie/movie.js
+++ public/views/movie/movie.js
@@ -9,12 +9,16 @@ app.config(function($routeProvider){
 })
 
 app.controller('movie', function($scope, $http){
-    var API_BASE_URL = 'https://api.themoviedb.org/';
-    var API_KEY = 'cec2b81b22b80f0a38ca51d2c39ef48b';
+    var API_BASE_URL = 'https://api.themoviedb.org/3';
+    var API_KEY = '?api_key=cec2b81b22b80f0a38ca51d2c39ef48b';
+    var API_BASE_IMAGE = "http://image.tmdb.org/t/p";
+    var API_IMG_SIZE = "/original"; 
+    
 	//$scope.movie = API_BASE_URL + '/3/movie/550?api_key=' + API_KEY;
-    $http.get(API_BASE_URL + '/3/movie/550?api_key=' + API_KEY)
+    $http.get(API_BASE_URL + '/movie/510' + API_KEY)
     .success(function(data){
         $scope.movie = data
+        $scope.poster = API_BASE_IMAGE + API_IMG_SIZE + data.poster_path;
         console.log(data);
     })
     .error(function(err){
